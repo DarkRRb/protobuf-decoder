7e777ad814cc092390ffcfbe9a4f7bf503cf19ec
darkrrb <177549718+DarkRRb@users.noreply.github.com>
2024-12-13 19:00:48 +0800

    Clean up default data

--- src/views.viewmodels/App.viewmodel.ts
+++ src/views.viewmodels/App.viewmodel.ts
@@ -1,20 +1,12 @@
 import { createSignal, Signal } from "solid-js";
 import { ProtoBufNode } from "../utilities/n_protobuf_decode/node";
-import { decode } from "../utilities/n_protobuf_decode/decoder";
-import { hexToBytes } from "../utilities/hex_util";
 
 export default class AppViewModel {
     private _hex: Signal<string> = createSignal("");
 
     private _tip: Signal<string> = createSignal("");
 
-    private _nodes: Signal<ProtoBufNode[]> = createSignal(
-        decode(
-            hexToBytes(
-                "09000000000000f83f150000c03f18b99497eefbffffffff0120f2b8dcc39a95c594e10128c7ebe8910c308ec7a3bce5eabaeb9e01388dd7d1a308409b8ec7f8cad5f5d63d4dc7353ac2518ee3885756ebd69e5d39cac5bd61721c77a8a91429e1680170007a076461726b72726282010200ff8a017309000000000000f83f150000c03f18b99497eefbffffffff0120f2b8dcc39a95c594e10128c7ebe8910c308ec7a3bce5eabaeb9e01388dd7d1a308409b8ec7f8cad5f5d63d4dc7353ac2518ee3885756ebd69e5d39cac5bd61721c77a8a91429e1680170007a076461726b72726282010200ff"
-            )
-        )
-    );
+    private _nodes: Signal<ProtoBufNode[]> = createSignal([]);
 
     public get hex(): string {
         return this._hex[0]();
